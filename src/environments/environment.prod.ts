d5e444e671b986fb4018a8a55a74e77d71fc4029
Joaquin <jo.madariagan@duocuc.cl>
2025-10-17 00:27:07 -0300

    cambios produccion 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
- API_URL: 'https://backend-psicolink.onrender.com/api'
+  production: true,
+  API_URL: 'https://backend-psicolink.onrender.com/api'
 };
