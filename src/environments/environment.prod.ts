3fbecd264660e83d756480215f5e78ba3753a04f
Fer <fernando.cavada.m@gmail.com>
2025-11-05 03:26:53 -0300

    /

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   API_URL: 'https://backend-psicolink.onrender.com/api',
   supabaseUrl: 'https://jnntkzclpqmkkwiqynfg.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpubnRremNscHFta2t3aXF5bmZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4NDQ5MDAsImV4cCI6MjA3NTQyMDkwMH0.nwXt0FEUYrXdCAn3wX70e7vFCDzqnjbks7NOXqlltJ8',
-  MP_URL: 'https://backend-psicolink.onrender.com/payments/create-preference'
+  MP_URL: 'https://backend-psicolink.onrender.com/payments/create-preference/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   API_URL: 'http://localhost:8000/api',
   supabaseUrl: 'https://jnntkzclpqmkkwiqynfg.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpubnRremNscHFta2t3aXF5bmZnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4NDQ5MDAsImV4cCI6MjA3NTQyMDkwMH0.nwXt0FEUYrXdCAn3wX70e7vFCDzqnjbks7NOXqlltJ8',
-  MP_URL: 'http://localhost:8000/payments/create-preference'
+  MP_URL: 'http://localhost:8000/payments/create-preference/'
 };
 
 /*
