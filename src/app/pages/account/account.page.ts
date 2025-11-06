61e1a50d128d00532c3dc13986ebe2808442596d
lucas javier cisternas morales <luc.cisternas@duocuc.cl>
2025-11-06 13:59:07 -0300

    miscitas agendarcita enlazado con zoom

--- src/app/pages/account/account.page.ts
+++ src/app/pages/account/account.page.ts
@@ -5,22 +5,53 @@ import {
 } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { Auth } from 'src/app/services/auth';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.page.html',
   styleUrls: ['./account.page.scss'],
   standalone: true,
+<<<<<<< HEAD
   imports: [CommonModule, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonButton],
+=======
+  imports: [
+    CommonModule,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonButtons,
+    IonButton,
+  ],
+>>>>>>> 2392221 (miscitas agendarcita enlazado con zoom)
 })
-
 export class AccountPage {
   role = localStorage.getItem('role') || '';
 
   constructor(private auth: Auth, private router: Router) {}
+
+  connectZoom() {
+    const clientId = environment.zoomClientId;
+    const redirectUri = 'http://127.0.0.1:8000/api/zoom/oauth/callback/';
+    const userId = localStorage.getItem('user_id');
+
+    if (!userId) {
+      console.error("⚠️ No hay user_id en localStorage. No se puede conectar Zoom.");
+      return;
+    }
+
+    const zoomAuthUrl = `https://zoom.us/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&state=${userId}`;
+    window.location.href = zoomAuthUrl;
+  }
 
   async logout() {
     await this.auth.logout();
     this.router.navigate(['/login']);
   }
 }
+
+
