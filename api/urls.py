9580ed7c34b98a4bfe43a05f769e0d46ee9ed45e
Joaquin <jo.madariagan@duocuc.cl>
2025-11-16 22:57:21 -0300

    logica admin, tickets, usuario

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
-from .views import ProfessionalDetailView, RegisterView, ProfileSetupView, AppointmentViewSet, ProfesionalSearchView, SpecialtyListView, LoginView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
 from .views import (
     ProfessionalDetailView, RegisterView, ProfileSetupView, AppointmentViewSet,
-    ProfesionalSearchView, SpecialtyListView
+    ProfesionalSearchView, SpecialtyListView, LoginView,
+    SupportTicketCreateView, UserTicketListView, UserTicketDetailView, SupportTicketReplyView,
+    AdminTicketListView, SupportTicketDetailView, AdminUserDetailView, AdminUserListView,
+    ProfessionalAvailabilityView
 )
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .serializers import CustomTokenObtainPairSerializer
 from .views_zoom import zoom_connect, zoom_callback
 
@@ -26,7 +27,15 @@ urlpatterns = [
     path('professionals/<int:user_id>/', ProfessionalDetailView.as_view(), name='professional-detail'),
     path("zoom/connect/", zoom_connect, name="zoom_connect"),
     path("zoom/oauth/callback/", zoom_callback, name="zoom_callback"),
-
+    path('support/tickets/', SupportTicketCreateView.as_view(), name='create-support-ticket'),
+    path('support/mis-tickets/', UserTicketListView.as_view(), name='user-ticket-list'),
+    path('support/mis-tickets/<int:pk>/', UserTicketDetailView.as_view({'get': 'retrieve'}), name='user-ticket-detail'),
+    path('support/tickets/<int:pk>/reply/', SupportTicketReplyView.as_view(), name='ticket-reply'),
+    path('support/admin/tickets/<int:pk>/', SupportTicketDetailView.as_view(), name='admin-ticket-detail'),
+    path('support/admin/tickets/', AdminTicketListView.as_view(), name='admin-ticket-list'),
+    path('admin/users/', AdminUserListView.as_view(), name='admin-user-list'),
+    path('admin/users/<int:pk>/', AdminUserDetailView.as_view(), name='admin-user-detail'),
+    path('pro/profile/availability/', ProfessionalAvailabilityView.as_view(), name='pro-availability'),
 
 ]
 
