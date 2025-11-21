44918bee993a79a7110b2d4e33f2d7d38b20a07a
Joaquin <jo.madariagan@duocuc.cl>
2025-11-21 00:50:25 -0300

    APK version 1.0.1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,9 +4,6 @@ const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'psicolink',
   webDir: 'www/browser',
-  server: {
-    androidScheme: 'https'
-  },
   plugins: {
     Keyboard: {
       resize: 'body',
